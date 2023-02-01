bd51a5be12c76845c75de732df19d6d4eb06b086
shiningsea <ski.britness@gmail.com>
2023-02-01 15:12:57 -0500

    update

--- app/components/StarterKit.tsx
+++ app/components/StarterKit.tsx
@@ -3,7 +3,7 @@ import { ReactElement } from "react";
 function StarterKit(): ReactElement {
     return (
         <article className="text-center mb-8">
-            <h2 className="text-4xl font-semibold app__title text-primary">Welcome to our <span  className="inline-block border-1 bg-secondary px-2 text-default">Vehicle Search</span></h2>
+            <h2 className="text-4xl font-semibold app__title text-primary">Welcome to our <span  className="inline-block border-1 bg-secondary px-2 text-primary">Vehicle Search</span></h2>
         </article>
     )
 }
